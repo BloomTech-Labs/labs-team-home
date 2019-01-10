baf2fe86fbad8d9994e86094a4690557e28ffbc1
Erik Kimsey <kimseyerik@gmail.com>
2019-01-10 17:01:23 -0500

    BAnner background gradient added

--- frontend/src/LandingView/styles/LogoBannerStyled.js
+++ frontend/src/LandingView/styles/LogoBannerStyled.js
@@ -6,7 +6,16 @@ const LogoBannerStyles = styled.div`
 	flex-flow: row;
 	/* height: 560px; */
 	height: 70vh;
-	background: rgba(255, 255, 255, 0.6);
+	/* background: rgba(255, 255, 255, 0.6); */
+	background: linear-gradient(
+		to bottom,
+		rgba(255, 255, 255, 0) 0%,
+		rgba(255, 255, 255, 0.3) 20%,
+		rgba(255, 255, 255, 0.6) 40%,
+		rgba(255, 255, 255, 0.6) 60%,
+		rgba(255, 255, 255, 0.3) 80%,
+		rgba(255, 255, 255, 0) 100%
+	);
 `;
 
 const BannerStyles = styled.div`
