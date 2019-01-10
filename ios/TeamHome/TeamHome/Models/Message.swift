07e53ea2ae39d941520f8f25f6991c7696b0b74b
Daniela Parra <dyparra47@gmail.com>
2019-01-10 17:26:07 -0500

    Finish all model files.

---
+++ ios/TeamHome/TeamHome/Models/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  TeamHome
+//
+//  Created by Daniela Parra on 1/10/19.
+//  Copyright © 2019 Daniela Parra. All rights reserved.
+//
+
+import Foundation
---
+++ ios/TeamHome/TeamHome/Models/Team.swift
@@ -0,0 +1,9 @@
+//
+//  Team.swift
+//  TeamHome
+//
+//  Created by Daniela Parra on 1/10/19.
+//  Copyright © 2019 Daniela Parra. All rights reserved.
+//
+
+import Foundation
