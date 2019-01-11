67cda278ea13021819f562f9e66a8101dbeaff0b
Daniela Parra <dyparra47@gmail.com>
2019-01-11 16:31:35 -0500

    Add all VC files for all views and updated storyboard.

---
+++ ios/TeamHome/TeamHome/View Controllers/ActivityCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ActivityCollectionViewCell.swift
+//  TeamHome
+//
+//  Created by Daniela Parra on 1/11/19.
+//  Copyright © 2019 Daniela Parra. All rights reserved.
+//
+
+import UIKit
+
+class ActivityCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ ios/TeamHome/TeamHome/View Controllers/MessageDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MessageDetailViewController.swift
+//  TeamHome
+//
+//  Created by Daniela Parra on 1/11/19.
+//  Copyright © 2019 Daniela Parra. All rights reserved.
+//
+
+import UIKit
+
+class MessageDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
