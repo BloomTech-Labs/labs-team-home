4a10d44f5c7ed6a04006f3fa7bc57ed8a686200d
david pok <david-pok>
2019-01-15 15:38:56 -0800

    seting up settings page

---
+++ frontend/src/SettingsView/containers/SettingsView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+
+export default class SettingsView extends Component {
+	render() {
+		return (
+			<div>
+				asdf
+				<h1>settings page</h1>
+			</div>
+		);
+	}
+}
