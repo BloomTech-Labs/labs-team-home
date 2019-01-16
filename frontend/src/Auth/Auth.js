4b0930d9fb0b65e4f086c71ad76e56776b1d3ae0
Erik Kimsey <kimseyerik@gmail.com>
2019-01-16 11:54:55 -0500

    Changes

--- frontend/src/Auth/Auth.js
+++ frontend/src/Auth/Auth.js
@@ -7,11 +7,14 @@ const options = {
 	},
 	auth: {
 		sso: false
-	}
+	},
+	redirectUrl: process.env.REACT_APP_REDIRECT_URL || '/',
+	responseType: 'token id_token',
+	scope: 'openid profile email'
 };
 
 /**
- * Auth0 {class}:
+ * Auth0 class:
  * creates an instance of Auth0Lock, using the Auth0 ClientID and Auth0 domain.
  * login() / signUp() - call the Auth0 Lock 'show()' method with their respective default screens, while displaying the listed social connections options.
  */
---
+++ frontend/src/Auth/redirect.js
@@ -0,0 +1,7 @@
+/**
+ * TODO:
+ * - check if user has account info (name?),
+ * - if account exists, redirect to dashboard,
+ * - if account does not exist, redirect to account settings
+ */
+function redirect() {}
