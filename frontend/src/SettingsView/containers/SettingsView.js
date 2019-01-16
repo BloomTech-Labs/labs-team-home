ad8a9df09ea3462d8be8fee5886308e5f5d7ccf3
david pok <david-pok>
2019-01-15 20:54:33 -0800

    adds most of the form for user account settings, adds tab to cycle through user settings and billing

--- frontend/src/SettingsView/containers/SettingsView.js
+++ frontend/src/SettingsView/containers/SettingsView.js
@@ -1,13 +1,102 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import SettingsTabs from '../components/SettingsTabs';
+import '../styles/SettingsStyles.css';
+import FormInput from '../components/FormInput';
+import FormCheckbox from '../components/FormCheckbox';
+import FormButton from '../components/FormButton';
 
-export default class SettingsView extends Component {
+const buttonStyle = {
+	margin: '10px 10px 10px 10px'
+};
+
+class SettingsView extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			newUser: {
+				email: '',
+				phone: '',
+				receiveOptions: [],
+				oldPassword: '',
+				newPassword: ''
+			},
+
+			receiveOptions: ['Emails?', 'Texts?']
+		};
+		// this.handleEmail = this.handleEmail.bind(this);
+		// this.handlephone = this.handlephone.bind(this);
+		// this.handleFormSubmit = this.handleFormSubmit.bind(this);
+		// this.handleClearForm = this.handleClearForm.bind(this);
+		// this.handleCheckBox = this.handleCheckBox.bind(this);
+		// this.handleInput = this.handleInput.bind(this);
+	}
+
 	render() {
 		return (
 			<div>
-				asdf
-				<h1>settings page</h1>
+				<h1>Team Name probably</h1>
+				<SettingsTabs>
+					<div label="Account Settings">
+						<form onSubmit={this.somesubmithandlerthing}>
+							<FormInput
+								inputType={'text'}
+								title={'Email'}
+								name={'email'}
+								// value={this.state.email}
+								placeholder={'Enter your email'}
+								// handleChange={this.someInputHandler}
+							/>{' '}
+							<FormInput
+								inputType={'number'}
+								name={'phone'}
+								title={'Phone Number'}
+								// value={this.state.phone}
+								placeholder={'Enter your phone number'}
+								// handleChange={this.probablyAnotherInputHandler}
+							/>{' '}
+							<FormCheckbox
+								title={'Receive these?'}
+								name={'receiveOptions'}
+								options={this.state.receiveOptions}
+								selectedOptions={this.state.newUser.receiveOptions}
+								// handleChange={this.someCheckBoxHandler}
+							/>{' '}
+							<FormInput
+								inputType={'password'}
+								name={'oldPassword'}
+								title={'Old Password'}
+								// value={this.state.oldPassword}
+								placeholder={'Enter your old password'}
+								// handleChange={this.probablyAnotherInputHandler}
+							/>{' '}
+							<FormInput
+								inputType={'password'}
+								name={'newPassword'}
+								title={'New Password'}
+								// value={this.state.newPassword}
+								placeholder={'Enter your new password'}
+								// handleChange={this.probablyAnotherInputHandler}
+							/>{' '}
+							<FormButton
+								// action={this.someHandleFormSubmit}
+								type={'primary'}
+								title={'Save'}
+								style={buttonStyle}
+							/>{' '}
+							<FormButton
+								// action={this.someLeaveTeamHandler}
+								type={'primary'}
+								title={'Leave Team'}
+								style={buttonStyle}
+							/>{' '}
+						</form>
+					</div>
+					<div label="Team Billing">THE STRIPE GOES HERE!</div>
+				</SettingsTabs>
 			</div>
 		);
 	}
 }
+
+export default SettingsView;
