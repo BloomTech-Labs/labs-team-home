ab2861fba372d8ae0d131b19a7ba8ec89355110f
david-pok <david.pok@gmail.com>
2019-01-17 14:28:06 -0800

    sets up backend for stripe

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,10 @@ const configureMiddleware = require('./config');
 const app = express();
 configureMiddleware(app);
 
+//stripe
+configureRoutes = require('./stripe/routes');
+configureRoutes(app);
+
 app.listen(port, err => {
 	if (err) {
 		console.error(err);
