2dc128b2f0943481f2ed1c201d5caf6165ab36ea
Charles Cortinas <CRCORTINAS@GMAIL.COM>
2019-01-17 15:19:39 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-// import LandingView from './LandingView/containers/LandingView';
+import LandingView from './LandingView/containers/LandingView';
 import { Route } from 'react-router-dom';
 import MessageBoard from './MessageBoard/components/MessageBoard';
 // import Dashboard from './DashboardView/containers/Dashboard';
@@ -12,7 +12,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				{/* <Route exact path="/" component={LandingView} />*/}
+				<Route exact path="/" component={LandingView} />
 				<Route path="/home" component={MessageBoard} />
 				{/* <AuthRoute path="/dashboard" component={Dashboard} /> */}
 				<Route path="/settings" component={SettingsView} />
