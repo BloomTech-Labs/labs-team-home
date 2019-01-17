f0db4a148f2de66f4a949c4ed98d0dce46460336
Charles Cortinas <crcortinas@gmail.com>
2019-01-17 11:42:28 -0600

    Splitting front end/back end

--- backend/index.js
+++ backend/index.js
@@ -5,20 +5,6 @@ const configureMiddleware = require('./config');
 const app = express();
 configureMiddleware(app);
 
-app.post('/invmail', (req, res) => {
-	const sgMail = require('@sendgrid/mail');
-	sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-	const msg = {
-		to: req.body.email,
-		from: 'sender@example.org',
-		subject: 'Hello world',
-		text: 'Hello plain world!',
-		html: '<p>Hello HTML world!</p>'
-	};
-	sgMail.send(msg);
-	res.status(200).json({ message: 'request recieved roger wilco boss man' });
-});
-
 app.listen(port, err => {
 	if (err) {
 		console.error(err);
