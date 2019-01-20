f80cc41d916a1fb59552d197a3c4a490b9487724
koennecker <koenneckerb@gmail.com>
2019-01-20 14:06:02 -0700

    Implement email updates for when new comments are added to a message

--- backend/graphql/resolvers/MsgComment.js
+++ backend/graphql/resolvers/MsgComment.js
@@ -1,6 +1,8 @@
 const Message = require('../../models/Message');
 const MsgComment = require('../../models/MsgComment');
 const { ValidationError } = require('apollo-server-express');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const msgCommentResolvers = {
 	Query: {
@@ -11,28 +13,64 @@ const msgCommentResolvers = {
 			MsgComment.find({ message: message }).populate('user message likes')
 	},
 	Mutation: {
-		addMsgComment: (_, { input }, { user: { _id } }) => {
-			const { message } = input;
-			return new MsgComment({ ...input, user: _id }).save().then(comment =>
-				Message.findOneAndUpdate(
-					// adds comment to parent Message
-					{ _id: message },
-					{ $push: { comments: [comment._id] } }
-				).then(() => comment.populate('user message likes').execPopulate())
-			);
+		addMsgComment: (_, { input }, { user: { _id, firstName, lastName } }) => {
+			const { content } = input;
+			return new MsgComment({ ...input, user: _id })
+				.save()
+				.then(async comment => {
+					const message = await Message.findOneAndUpdate(
+						// adds comment to parent Message
+						{ _id: message },
+						{ $push: { comments: [comment._id] } },
+						{ new: true }
+					).populate('team subscribedUsers');
+					const emails = message.subscribedUsers.map(
+						// creates an array of emails of subscribed users, if their email is on file and the user isn't the one adding the message
+						user =>
+							user.toggles.recieveEmails &&
+							user.email &&
+							user._id !== _id &&
+							user.email
+					);
+					await sgMail.send({
+						// notifies subscribed users of the new comment
+						to: emails,
+						from: 'sender@example.org',
+						subject: `The message ${
+							message.title
+						} has a new comment from ${firstName} ${lastName} on your team ${
+							message.team.name
+						}`,
+						text: `${content}`,
+						html: /* HTML */ `
+							<h1>${message.team.name}</h1>
+							<div>
+								<h2>Message:</h2>
+								<h3>${message.title}</h3>
+								<p>${message.content}</p>
+							</div>
+							<div>
+								<h2>New comment:</h2>
+								<p>${content}</p>
+							</div>
+						`
+					});
+					return comment.populate('user message likes').execPopulate();
+				});
 		},
-		updateMsgComment: async (_, { input }) => {
+		updateMsgComment: (_, { input }) => {
 			const { id } = input;
-			const comment = await MsgComment.findById(id);
-			if (comment) {
-				return MsgComment.findOneAndUpdate(
-					{ _id: id },
-					{ $set: input },
-					{ new: true }
-				).populate('user message likes');
-			} else {
-				throw new ValidationError('Comment does not exist');
-			}
+			return MsgComment.findById(id).then(comment => {
+				if (comment) {
+					return MsgComment.findOneAndUpdate(
+						{ _id: id },
+						{ $set: input },
+						{ new: true }
+					).populate('user message likes');
+				} else {
+					throw new ValidationError('Comment does not exist');
+				}
+			});
 		},
 		deleteMsgComment: (_, { input: { id } }) =>
 			MsgComment.findById(id).then(async comment => {
