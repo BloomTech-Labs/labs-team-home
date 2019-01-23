f150993ead3b4128f84d0b5cc05ccca8a5cf05d3
zacharyarney <zachary.arney@gmail.com>
2019-01-22 17:11:27 -0800

    Adds acceptedFileTypes prop to FilePond

--- frontend/src/MessageBoard/components/AddMessage.js
+++ frontend/src/MessageBoard/components/AddMessage.js
@@ -146,71 +146,72 @@ export default function AddMessage(props) {
 								/>
 							</label>
 							<ImageUploadContainer>
-							<FilePond
-								allowMultiple={true}
-								server={{
-									url: `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
-									process: (
-										fieldName,
-										file,
-										metadata,
-										load,
-										error,
-										progress,
-										abort
-									) => {
-										// fieldName is the name of the input field
-										// file is the actual file object to send
-										const formData = new FormData();
-										formData.append(fieldName, file, file.name);
-										formData.append('file', file);
-										formData.append('upload_preset', uploadPreset);
-										formData.append('api_key', apiKey);
+								<FilePond
+									allowMultiple={true}
+									acceptedFileTypes="image/jpeg, image/png, image/gif"
+									server={{
+										url: `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+										process: (
+											fieldName,
+											file,
+											metadata,
+											load,
+											error,
+											progress,
+											abort
+										) => {
+											// fieldName is the name of the input field
+											// file is the actual file object to send
+											const formData = new FormData();
+											formData.append(fieldName, file, file.name);
+											formData.append('file', file);
+											formData.append('upload_preset', uploadPreset);
+											formData.append('api_key', apiKey);
 
-										const request = new XMLHttpRequest();
-										request.open(
-											'POST',
-											`https://${apiKey}:${apiSecret}@api.cloudinary.com/v1_1/${cloudName}/image/upload`
-										);
+											const request = new XMLHttpRequest();
+											request.open(
+												'POST',
+												`https://${apiKey}:${apiSecret}@api.cloudinary.com/v1_1/${cloudName}/image/upload`
+											);
 
-										// Should call the progress method to update the progress to 100% before calling load
-										// Setting computable to false switches the loading indicator to infinite mode
-										request.upload.onprogress = e => {
-											progress(e.lengthComputable, e.loaded, e.total);
-										};
+											// Should call the progress method to update the progress to 100% before calling load
+											// Setting computable to false switches the loading indicator to infinite mode
+											request.upload.onprogress = e => {
+												progress(e.lengthComputable, e.loaded, e.total);
+											};
 
-										// Should call the load method when done and pass the returned server file id
-										// this server file id is then used later on when reverting or restoring a file
-										// so your server knows which file to return without exposing that info to the client
-										request.onload = function() {
-											if (request.status >= 200 && request.status < 300) {
-												// the load method accepts either a string (id) or an object
-												const response = JSON.parse(request.response);
-												console.log(response);
-												//add new url to the images array in preparation of creating new message
-												images.push(response.secure_url);
-												load(request.responseText);
-											} else {
-												// Can call the error method if something is wrong, should exit after
-												error('oh no');
-											}
-										};
+											// Should call the load method when done and pass the returned server file id
+											// this server file id is then used later on when reverting or restoring a file
+											// so your server knows which file to return without exposing that info to the client
+											request.onload = function() {
+												if (request.status >= 200 && request.status < 300) {
+													// the load method accepts either a string (id) or an object
+													const response = JSON.parse(request.response);
+													console.log(response);
+													//add new url to the images array in preparation of creating new message
+													images.push(response.secure_url);
+													load(request.responseText);
+												} else {
+													// Can call the error method if something is wrong, should exit after
+													error('oh no');
+												}
+											};
 
-										request.send(formData);
+											request.send(formData);
 
-										// Should expose an abort method so the request can be cancelled
-										return {
-											abort: () => {
-												// This function is entered if the user has tapped the cancel button
-												request.abort();
+											// Should expose an abort method so the request can be cancelled
+											return {
+												abort: () => {
+													// This function is entered if the user has tapped the cancel button
+													request.abort();
 
-												// Let FilePond know the request has been cancelled
-												abort();
-											}
-										};
-									}
-								}}
-							/>
+													// Let FilePond know the request has been cancelled
+													abort();
+												}
+											};
+										}
+									}}
+								/>
 							</ImageUploadContainer>
 							<input type="submit" value="Submit" />
 						</form>
