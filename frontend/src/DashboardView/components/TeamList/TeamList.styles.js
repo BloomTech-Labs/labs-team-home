b991912626e99a6afcb1b044975ed1c411f92959
Erik Kimsey <kimseyerik@gmail.com>
2019-01-24 12:49:55 -0500

    Cleaning and small fix

--- frontend/src/DashboardView/components/TeamList/TeamList.styles.js
+++ frontend/src/DashboardView/components/TeamList/TeamList.styles.js
@@ -26,61 +26,59 @@ export const Container = styled.div`
 `;
 
 const Form = styled.div`
-  display:flex;
-  flex-flow:column;
-  /* justify-content:center; */
-  justify-items:center;
-  width: 100%;  
-  margin: 3% auto;
+	display: flex;
+	flex-flow: column;
+	/* justify-content:center; */
+	justify-items: center;
+	width: 100%;
+	margin: 3% auto;
 	font-family: Comfortaa;
 	color: #fff;
-  /* border-top: solid 1px #ff8c63; */
-  border-radius:5px;
-  ${mediaQueryFor.mdDevice`
+	/* border-top: solid 1px #ff8c63; */
+	border-radius: 5px;
+	${mediaQueryFor.mdDevice`
       width:;
     `}
-  h3 {
-    font-family: Comfortaa;
+	h3 {
+		font-family: Comfortaa;
 		color: #fff;
 		margin: 0 auto;
-  }
-  label {
-    display:flex;
-    flex-flow:column;
-    
-    input {
-      height:50px;
-      padding:0 0 0 30px;
-      font-size:2rem;
-      background-color: #17151B;
-      font-family: Comfortaa;
-      color: #FFD17C;
-    }
+	}
+	label {
+		display: flex;
+		flex-flow: column;
 
-  }
-  button {
-    width:100%;
-    display: inline-block;
-    border: none;
-    /* padding: 1rem 2rem; */
-    margin: 0;
-    text-decoration: none;
-    background-color: #17151B;
-    color: #FFD17C;
-    font-family: sans-serif;
-    font-size:2.2rem;
-    /* line-height: 1; */
-    cursor: pointer;
-    text-align: center;
-    transition: background-color 250ms ease-in-out, transform 150ms ease;
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    :hover {
-      background-color:#FFD17C;
-      color:#FFD17C;
-    }
-  }
-}
+		input {
+			height: 50px;
+			padding: 0 0 0 30px;
+			font-size: 2rem;
+			background-color: #17151b;
+			font-family: Comfortaa;
+			color: #ffd17c;
+		}
+	}
+	button {
+		width: 100%;
+		display: inline-block;
+		border: none;
+		/* padding: 1rem 2rem; */
+		margin: 0;
+		text-decoration: none;
+		background-color: #17151b;
+		color: #ffd17c;
+		font-family: sans-serif;
+		font-size: 2.2rem;
+		/* line-height: 1; */
+		cursor: pointer;
+		text-align: center;
+		transition: background-color 250ms ease-in-out, transform 150ms ease;
+		-webkit-appearance: none;
+		-moz-appearance: none;
+		:hover {
+			background-color: #ffd17c;
+			color: #ffd17c;
+		}
+	}
 `;
 
 const TeamsList = styled.div`
