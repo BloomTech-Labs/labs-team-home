8a2b3347a5a669a4690dfd652dc7094f28e2ae96
Daniela Parra <dyparra47@gmail.com>
2019-01-28 11:47:38 -0500

    Add new image assets and clean up UI for landing page

--- ios/TeamHome/TeamHome/Resources/AppDelegate.swift
+++ ios/TeamHome/TeamHome/Resources/AppDelegate.swift
@@ -7,14 +7,12 @@
 //
 
 import UIKit
-import Apollo
 import Lock
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
