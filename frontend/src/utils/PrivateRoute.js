be34b1201c938ef02f0f41dd20e76f4f7c20348e
koennecker <koenneckerb@gmail.com>
2019-01-27 18:13:59 -0700

    Remove unused import from PrivateRoute and add comment

--- frontend/src/utils/PrivateRoute.js
+++ frontend/src/utils/PrivateRoute.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
-import { graphql, Query } from 'react-apollo';
-import gql from 'graphql-tag';
-import * as query from '../constants/queries';
+import { Query } from 'react-apollo';
+import { CURRENT_USER } from '../constants/queries';
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
 	<Route
@@ -14,11 +13,12 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
 					jwt_decode(localStorage.token).exp > Math.floor(Date.now() / 1000) // checks if token is a JWT and if it isn't expired
 				) {
 					return (
-						<Query query={query.CURRENT_USER}>
+						<Query query={CURRENT_USER}>
 							{({ loading, error, data: { currentUser } }) => {
 								if (loading) return <p>Loading...</p>;
 								if (error) return <p>Error :(</p>;
 								if (currentUser) {
+									// checks if user exists in the database, if not redirects to settings page for account creation
 									return <Component {...props} currentUser={currentUser} />;
 								} else if (props.location.pathname === '/settings') {
 									return <Component {...props} currentUser={null} />;
