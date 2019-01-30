d622e3a318e3414bae21f9446797dbd3b35592ba
zacharyarney <zachary.arney@gmail.com>
2019-01-29 19:14:32 -0800

    Imports FilePond component and all its dependencies

--- frontend/src/SettingsView/containers/SettingsView.js
+++ frontend/src/SettingsView/containers/SettingsView.js
@@ -9,9 +9,34 @@ import FormButton from '../components/forms/FormButton';
 import BillingView from '../BillingView/BillingView';
 import * as mutation from '../../constants/mutations';
 import * as query from '../../constants/queries';
+import { FilePond, registerPlugin } from 'react-filepond';
+import FilePondPluginFileTypeValidation from 'filepond-plugin-file-validate-type';
+import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
+import FilePondPluginImageResize from 'filepond-plugin-image-resize';
+import FilePondPluginImageTransform from 'filepond-plugin-image-transform';
+// image preview not working
+// import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import 'filepond/dist/filepond.min.css';
 
-import SettingsContainer from '../styles/container.styles';
+import SettingsContainer, {
+	StyledAvatar,
+	ImageFigure,
+	AvatarUploadContainer
+} from '../styles/container.styles';
 import Auth0 from '../../Auth/Auth';
+
+registerPlugin(
+	FilePondPluginImageExifOrientation,
+	FilePondPluginFileTypeValidation,
+	FilePondPluginImageResize,
+	FilePondPluginImageTransform
+	//FilePondPluginImagePreview
+);
+
+const uploadPreset = process.env.REACT_APP_UPLOAD_PRESET;
+const apiKey = process.env.REACT_APP_API_KEY;
+const apiSecret = process.env.REACT_APP_API_SECRET;
+const cloudName = process.env.REACT_APP_CLOUD_NAME;
 
 const buttonStyle = {
 	margin: '10px 10px 10px 10px'
@@ -31,6 +56,7 @@ class SettingsView extends Component {
 			selected: [],
 			toggles: {}
 		};
+		const avatar = '';
 	}
 
 	componentDidMount() {
@@ -88,6 +114,10 @@ class SettingsView extends Component {
 		});
 	};
 
+	handleImageUpload = url => {
+		this.setState({ avatar: url });
+	};
+
 	render() {
 		const { currentUser, history } = this.props;
 		return currentUser ? (
@@ -104,11 +134,102 @@ class SettingsView extends Component {
 											variables: {
 												email: this.state.email,
 												phoneNumber: this.state.phoneNumber,
-												toggles: this.state.toggles
+												toggles: this.state.toggles,
+												avatar: this.avatar
 											}
 										});
 									}}
 								>
+									<AvatarUploadContainer>
+										<ImageFigure>
+											<StyledAvatar
+												src={this.props.currentUser.avatar}
+												alt="User avatar"
+											/>
+											<figcaption>User avatar</figcaption>
+										</ImageFigure>
+										<FilePond
+											ref={ref => (this.pond = ref)}
+											//files={this.state.image}
+											allowMultiple={false}
+											acceptedFileTypes="image/jpeg, image/png, image/gif"
+											imageResizeTargetWidth={1280}
+											imageResizeTargetHeight={800}
+											imageResizeMode="contain"
+											imageResizeUpscale={false}
+											onupdatefiles={fileItems => {
+												fileItems.map(fileItem =>
+													console.log('FILE ITEM', fileItem)
+												);
+												// Set current file objects to this.state
+												this.setState({
+													files: fileItems.map(fileItem => fileItem.file)
+												});
+											}}
+											server={{
+												url: `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+												process: (
+													fieldName,
+													file,
+													metadata,
+													load,
+													error,
+													progress,
+													abort
+												) => {
+													// fieldName is the name of the input field
+													// file is the actual file object to send
+													const formData = new FormData();
+													formData.append(fieldName, file, file.name);
+													formData.append('file', file);
+													formData.append('upload_preset', uploadPreset);
+													formData.append('api_key', apiKey);
+
+													const request = new XMLHttpRequest();
+													request.open(
+														'POST',
+														`https://${apiKey}:${apiSecret}@api.cloudinary.com/v1_1/${cloudName}/image/upload`
+													);
+
+													// Should call the progress method to update the progress to 100% before calling load
+													// Setting computable to false switches the loading indicator to infinite mode
+													request.upload.onprogress = e => {
+														progress(e.lengthComputable, e.loaded, e.total);
+													};
+
+													// Should call the load method when done and pass the returned server file id
+													// this server file id is then used later on when reverting or restoring a file
+													// so your server knows which file to return without exposing that info to the client
+													request.onload = function() {
+														if (request.status >= 200 && request.status < 300) {
+															// the load method accepts either a string (id) or an object
+															const response = JSON.parse(request.response);
+															this.avatar = response.secure_url;
+															console.log('THIS DOT AVATAR', this.avatar);
+															//add new url to the images array in preparation of creating new message
+															load(request.responseText);
+														} else {
+															// Can call the error method if something is wrong, should exit after
+															error('oh no');
+														}
+													};
+
+													request.send(formData);
+
+													// Should expose an abort method so the request can be cancelled
+													return {
+														abort: () => {
+															// This function is entered if the user has tapped the cancel button
+															request.abort();
+
+															// Let FilePond know the request has been cancelled
+															abort();
+														}
+													};
+												}
+											}}
+										/>
+									</AvatarUploadContainer>
 									<FormInput
 										inputType="text"
 										name={'firstName'}
