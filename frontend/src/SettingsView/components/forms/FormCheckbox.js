abd8925769be9d7c90c5fc5f41c30b490ef7ebc6
zacharyarney <zachary.arney@gmail.com>
2019-01-30 21:10:49 -0800

    Move styles to container.styles.js

--- frontend/src/SettingsView/components/forms/FormCheckbox.js
+++ frontend/src/SettingsView/components/forms/FormCheckbox.js
@@ -2,35 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import mediaQueryFor from '../../../_global_styles/responsive_querie';
 
-const FormCheckboxStyles = styled.div`
-	display: flex;
-	flex-flow: row;
-	padding: 1% 4%;
-	label {
-		width: 25%;
-		margin-right: 3%;
-	}
-	input {
-		width: 2%;
-	}
-	${mediaQueryFor.smDevice`
-    display: flex;
-    flex-direction: column;
-    margin: 0 auto;
-    font-size:1.5rem;
-    label {
-      width:80%;
-      display:flex;
-      flex-flow:row;
-      justify-content: space-between;
-
-      input {
-        width: 20%;
-        height:50px
-      }
-    }
-    `}
-`;
 
 const FormCheckbox = props => {
 	return (
--- frontend/src/SettingsView/components/forms/FormInput.js
+++ frontend/src/SettingsView/components/forms/FormInput.js
@@ -1,37 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import mediaQueryFor from '../../../_global_styles/responsive_querie';
+import { FormInputStyles, StyledInput } from '../../styles/container.styles'
 
-const FormInputStyles = styled.div`
-	display: flex;
-	flex-flow: row;
-	padding: 1%;
-	label {
-		width: 25%;
-	}
-	input {
-		width: 25%;
-	}
-	${mediaQueryFor.smDevice`
-    display: flex;
-    flex-direction: column;
-    margin: 0 auto;
-    font-size:1.5rem;
-    margin: 3% 0 2% 0;
-    label {
-      width: 98%;
-    }
-    input {
-      width: 98%;
-      height: 50px;
-			padding: 0 0 0 10px;
-			font-size: 1.2rem;
-			background-color:#F1FCEF;
-			font-family: Comfortaa;
-			color: #ffd17c;
-    }
-    `}
-`;
+
 
 const FormInput = props => {
 	return (
