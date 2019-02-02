f19110e5cb3734c12d3f10cec9482497b4a85c44
Erik Kimsey <kimseyerik@gmail.com>
2019-02-02 14:14:15 -0500

    Using react transition group, but hitting blocker

--- frontend/src/app-styles.js
+++ frontend/src/app-styles.js
@@ -4,6 +4,21 @@ const AppStyles = styled.div`
 	width: 100%;
 	margin: 0 auto;
 	height: 100vh;
+	.fade-enter {
+		opacity: 0.01;
+	}
+	.fade-enter.fade-enter-active {
+		opacity: 1;
+		transition: opacity 300ms ease-in;
+	}
+	.fade-exit {
+		opacity: 1;
+	}
+
+	.fade-exit.fade-exit-active {
+		opacity: 0.01;
+		transition: opacity 300ms ease-in;
+	}
 `;
 
 export default AppStyles;
