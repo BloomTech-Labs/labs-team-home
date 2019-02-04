c82ca7ea535b7759f2bdd1c39321ef6d99b25437
koennecker <koenneckerb@gmail.com>
2019-02-04 14:43:12 -0700

    Update comment editing behavior - Instead of re-using the form for editing comments at the bottom, now the text where the comment is displayed is turned into an input field when the 'edit comment' button is pushed - Fixed a bug caused by the use of refs with the Material UI textfield

--- frontend/src/MessageBoard/components/mutations/comments.js
+++ frontend/src/MessageBoard/components/mutations/comments.js
@@ -1,4 +1,5 @@
 import { graphql } from 'react-apollo';
+import * as query from '../../../constants/queries';
 import {
 	ADD_COMMENT,
 	UPDATE_COMMENT,
@@ -9,7 +10,33 @@ import {
 
 export const addComment = graphql(ADD_COMMENT);
 
-export const updateComment = graphql(UPDATE_COMMENT);
+const updateCommentOptions = {
+	props: ({ ownProps: { message }, mutate }) => ({
+		updateMsgComment: input =>
+			mutate({
+				variables: input,
+				update: (cache, { data: { updateMsgComment } }) => {
+					const { findMsgCommentsByMessage } = cache.readQuery({
+						query: query.FIND_COMMENTS_BY_MESSAGE,
+						variables: { message: message._id }
+					});
+					cache.writeQuery({
+						query: query.FIND_COMMENTS_BY_MESSAGE,
+						variables: { message: message._id },
+						data: {
+							findMsgCommentsByMessage: findMsgCommentsByMessage.map(comment =>
+								comment._id === updateMsgComment._id
+									? updateMsgComment
+									: comment
+							)
+						}
+					});
+				}
+			})
+	})
+};
+
+export const updateComment = graphql(UPDATE_COMMENT, updateCommentOptions);
 
 export const deleteComment = graphql(DELETE_COMMENT);
 
