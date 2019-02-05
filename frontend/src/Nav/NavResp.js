cffa2c27195ce7e3da087356c3b63975a9ff28ef
Erik Kimsey <kimseyerik@gmail.com>
2019-02-05 09:41:09 -0500

    Began responsive navbar

---
+++ frontend/src/Nav/NavResp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import NavBar, { StyledLink, TextIMG } from './styles/index';
+import styled from 'styled-components';
+// import Button from '../components/SignInUpButton';
+import textLogo from '../assets/TH_text_filled.svg';
+
+const BtnContainer = styled.div`
+	max-width: 400px;
+	width: 100%;
+	display: flex;
+	flex-flow: row;
+`;
---
+++ node_modules/@babel/runtime/helpers/esm/getPrototypeOf.js
@@ -0,0 +1,8 @@
+export default function _getPrototypeOf(o) {
+  _getPrototypeOf = Object.setPrototypeOf
+    ? Object.getPrototypeOf
+    : function _getPrototypeOf(o) {
+        return o.__proto__ || Object.getPrototypeOf(o);
+      };
+  return _getPrototypeOf(o);
+}
