1e730a9c7bdfb5db31a84d956827ba06140481a1
Civoremo <omerovic_n@hotmail.com>
2019-02-14 13:36:44 -0500

    uncommented previous code commit and removed part of a url in code that was not matching our Auth0

--- backend/config/apollo-server.js
+++ backend/config/apollo-server.js
@@ -13,44 +13,44 @@ const schema = makeExecutableSchema({
 	resolvers
 });
 // testing my push/pull request and branch auth. sorry for the frivolous pull request.
-// const context = async ({ req }) => {
-// 	let currentUser;
-// 	const token = req.headers.authorization;
-// 	const client = jwksClient({
-// 		jwksUri: `${AUTH0_DOMAIN}/.well-known/jwks.json`
-// 	});
-// 	const getKey = (header, cb) =>
-// 		client.getSigningKey(header.kid, (err, key) => {
-// 			let signingKey = key.publicKey || key.rsaPublicKey;
-// 			cb(null, signingKey);
-// 		});
-// 	const options = {
-// 		aud: `https://team-home-2-graphql-mongodb.herokuapp.com/`,
-// 		iss: `${AUTH0_DOMAIN}/api/v2/`,
-// 		algorithms: ['RS256']
-// 	};
-// 	try {
-// 		currentUser = await new Promise((resolve, reject) =>
-// 			jwt.verify(token, getKey, options, (err, decoded) => {
-// 				if (err) {
-// 					reject(err);
-// 				}
-// 				return (
-// 					decoded &&
-// 					UserModel.findOne({ authId: decoded.sub }).then(
-// 						existingUser =>
-// 							existingUser
-// 								? resolve(existingUser) // adds user to Apollo context, giving all resolvers access to the user
-// 								: resolve(decoded) // adds the decoded token to the Apollo context
-// 					)
-// 				);
-// 			})
-// 		);
-// 	} catch (e) {
-// 		throw new AuthenticationError(`${e}`);
-// 	}
-// 	return { user: currentUser };
-// };
+const context = async ({ req }) => {
+	let currentUser;
+	const token = req.headers.authorization;
+	const client = jwksClient({
+		jwksUri: `${AUTH0_DOMAIN}/.well-known/jwks.json`
+	});
+	const getKey = (header, cb) =>
+		client.getSigningKey(header.kid, (err, key) => {
+			let signingKey = key.publicKey || key.rsaPublicKey;
+			cb(null, signingKey);
+		});
+	const options = {
+		aud: `https://team-home-2-graphql-mongodb.herokuapp.com/`,
+		iss: `${AUTH0_DOMAIN}/`,
+		algorithms: ['RS256']
+	};
+	try {
+		currentUser = await new Promise((resolve, reject) =>
+			jwt.verify(token, getKey, options, (err, decoded) => {
+				if (err) {
+					reject(err);
+				}
+				return (
+					decoded &&
+					UserModel.findOne({ authId: decoded.sub }).then(
+						existingUser =>
+							existingUser
+								? resolve(existingUser) // adds user to Apollo context, giving all resolvers access to the user
+								: resolve(decoded) // adds the decoded token to the Apollo context
+					)
+				);
+			})
+		);
+	} catch (e) {
+		throw new AuthenticationError(`${e}`);
+	}
+	return { user: currentUser };
+};
 
 const server = new ApolloServer({
 	schema,
