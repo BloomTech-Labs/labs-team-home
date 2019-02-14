73283f6eec86dc78c12a3ff5737a8c9a0a47edc0
steelworx <eileencuevas62@gmail.com>
2019-02-14 13:24:44 -0500

    commented out jwt-decode for PublicRoute

--- frontend/src/utils/PublicRoute.js
+++ frontend/src/utils/PublicRoute.js
@@ -6,17 +6,17 @@ const PublicRoute = ({ component: Component, ...rest }) => (
 	<Route
 		{...rest}
 		render={props => {
-			if (localStorage.token) {
-				if (
-					jwt_decode(localStorage.token).exp > Math.floor(Date.now() / 1000) // checks if token is a JWT and if it isn't expired
-				) {
+			// if (localStorage.token) {
+			// 	if (
+			// 		jwt_decode(localStorage.token).exp > Math.floor(Date.now() / 1000) // checks if token is a JWT and if it isn't expired
+			// 	) {
 					return <Redirect to="/dashboard" />;
-				} else {
-					localStorage.removeItem('token');
-					return <Component {...props} />;
-				}
-			}
-			return <Component {...props} />;
+			// 	} else {
+			// 		localStorage.removeItem('token');
+			// 		return <Component {...props} />;
+			// 	}
+			// }
+			// return <Component {...props} />;
 		}}
 	/>
 );
