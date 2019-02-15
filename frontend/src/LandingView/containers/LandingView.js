9f767809fa54fd1b771afcc62a0677e9b7905759
steelworx <eileencuevas62@gmail.com>
2019-02-14 22:50:03 -0500

    added comments to LandingView.js

--- frontend/src/LandingView/containers/LandingView.js
+++ frontend/src/LandingView/containers/LandingView.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import LogoBanner from '../components/LandingLogoBanner';
-import Particles from 'react-particles-js';
-import particles from '../../animated/particles.json';
-import RespNavBAr from '../../Nav/NavResp';
+import LogoBanner from '../components/LandingLogoBanner'; // component
+import Particles from 'react-particles-js'; // fancy animations
+import particles from '../../animated/particles.json'; // settings for fancy animations
+import RespNavBAr from '../../Nav/NavResp'; // component
 
 const styles = {
 	position: 'fixed',
@@ -22,9 +22,9 @@ export default class LandingView extends Component {
 	render() {
 		return (
 			<div>
-				<RespNavBAr />
-				<Particles params={particles} style={styles} />
-				<LogoBanner />
+				<RespNavBAr /> {/* The navigation bar for specifically the landing page */}
+				<Particles params={particles} style={styles} /> {/* The cool animation effct */}
+				<LogoBanner /> {/** Landing Page Content */}
 			</div>
 		);
 	}
