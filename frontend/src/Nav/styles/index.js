a758d9c47d0cf50e4e8ecc0485fd8ae691abf0e5
steelworx <eileencuevas62@gmail.com>
2019-02-18 16:47:58 -0500

    removed animation from nav bar for better user experience

--- frontend/src/Nav/styles/index.js
+++ frontend/src/Nav/styles/index.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import mediaQueryFor from '../../_global_styles/responsive_querie';
 
+/* styling for the navbar buttons */
 const StyledLink = styled(Link)`
 	@import url('https://fonts.googleapis.com/css?family=Comfortaa|Righteous');
 	font-family: Comfortaa;
@@ -34,8 +35,10 @@ const TextIMG = styled.img`
 	margin-left: 20px;
 `;
 
+/* this styling doesn't ever seem to be used, but let's change the background color just in case*/
 const NavBar = styled.div`
-	background: linear-gradient(100deg, #17151b, rgba(222, 59, 97, 1), #17151b);
+	/* background: linear-gradient(100deg, #17151b, rgba(222, 59, 97, 1), #17151b); */
+	background-color: #17151b;
 	background-size: 600% 600%;
 	width: 100%;
 	position: fixed;
@@ -48,7 +51,7 @@ const NavBar = styled.div`
 		background-color: #17151b;
 	}
 
-	-webkit-animation: AnimationName 26s ease infinite;
+	/* -webkit-animation: AnimationName 26s ease infinite;
 	-moz-animation: AnimationName 26s ease infinite;
 	animation: AnimationName 26s ease infinite;
 	@-webkit-keyframes AnimationName {
@@ -83,11 +86,13 @@ const NavBar = styled.div`
 		100% {
 			background-position: 0% 50%;
 		}
-	}
+	} */
 `;
 
 const RespNav = styled.div`
-	background: linear-gradient(100deg, #17151b, rgba(222, 59, 97, 1), #17151b);
+	/* removed the gradient background as it's distracting for users */
+	/* background: linear-gradient(100deg, #17151b, rgba(222, 59, 97, 1), #17151b); */
+	background-color: #3e3145;
 	background-size: 600% 600%;
 	width: 100%;
 	position: fixed;
@@ -99,6 +104,7 @@ const RespNav = styled.div`
 	flex-flow: row;
 	z-index: 1001;
 	text-decoration: none;
+	margin-bottom: 20px;
 
 	justify-content: space-around;
 	.navbar {
@@ -116,7 +122,6 @@ const RespNav = styled.div`
 		}
 	}
 	${mediaQueryFor.mdDevice`
-    padding: 0;
     display:flex;
     flex-flow: column;
     justify-content: space-between;
@@ -129,7 +134,6 @@ const RespNav = styled.div`
     }
   `}
 	${mediaQueryFor.smDevice`
-    padding: 0;
     display:flex;
     flex-flow: column;
     justify-content: space-between;
@@ -141,43 +145,6 @@ const RespNav = styled.div`
       border-top: solid 1px rgba(0,0,0,0.3);
     }
   `}
-
-	-webkit-animation: AnimationName 26s ease infinite;
-	-moz-animation: AnimationName 26s ease infinite;
-	animation: AnimationName 26s ease infinite;
-	@-webkit-keyframes AnimationName {
-		0% {
-			background-position: 0% 50%;
-		}
-		50% {
-			background-position: 100% 50%;
-		}
-		100% {
-			background-position: 0% 50%;
-		}
-	}
-	@-moz-keyframes AnimationName {
-		0% {
-			background-position: 0% 50%;
-		}
-		50% {
-			background-position: 100% 50%;
-		}
-		100% {
-			background-position: 0% 50%;
-		}
-	}
-	@keyframes AnimationName {
-		0% {
-			background-position: 0% 50%;
-		}
-		50% {
-			background-position: 100% 50%;
-		}
-		100% {
-			background-position: 0% 50%;
-		}
-	}
 `;
 
 export default NavBar;
