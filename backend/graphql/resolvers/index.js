3fc9c375540a6e54ef727254d1e9c51d80904777
steelworx <eileencuevas62@gmail.com>
2019-02-18 20:04:05 -0500

    removed document and folder items from backend

--- backend/graphql/resolvers/index.js
+++ backend/graphql/resolvers/index.js
@@ -3,17 +3,6 @@ const User = require('./User');
 const Message = require('./Message');
 const MsgComment = require('./MsgComment');
 const Team = require('./Team');
-const Folder = require('./Folder');
-const Document = require('./Document');
 const { merge } = require('lodash');
 
-module.exports = merge(
-	{},
-	Tag,
-	User,
-	Team,
-	MsgComment,
-	Message,
-	Folder,
-	Document
-);
+module.exports = merge({}, Tag, User, Team, MsgComment, Message);
--- backend/graphql/schema/index.js
+++ backend/graphql/schema/index.js
@@ -3,7 +3,5 @@ const Tag = require('./Tag');
 const Team = require('./Team');
 const MsgComment = require('./MsgComment');
 const User = require('./User');
-const Folder = require('./Folder');
-const Document = require('./Document');
 
-module.exports = [Tag, Message, Team, MsgComment, User, Folder, Document];
+module.exports = [Tag, Message, Team, MsgComment, User];
