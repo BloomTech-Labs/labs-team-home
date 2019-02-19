8b8a3e77dbd9b574568d381fc59575690b733e43
Civoremo <omerovic_n@hotmail.com>
2019-02-18 22:32:24 -0500

    Folder model/schema/resolver

--- backend/graphql/resolvers/Folder.js
+++ backend/graphql/resolvers/Folder.js
@@ -1,14 +1,49 @@
 require('dotenv').config();
 const Folder = require('../../models/Folder');
+const Document = require('../../models/Document');
 
 const { ValidationError } = require('apollo-server-express');
 
 const folderResolver = {
 	Query: {
-		// folders: () => Folder.find().populate('user team')
-		folders() {
-			return [{ id: 1, title: 'some title' }];
+		folders: () => Folder.find().populate('user team documents'),
+		findFolders: (_, { input: { id } }) =>
+			Folder.findById(id)
+				.populate('user team documents')
+				.then(folder => folder),
+		findFoldersByTeam: (_, { input: { team } }) => {
+			console.log(team);
+			Folder.find({ team: team }).populate('user team documents');
 		}
+	},
+	Mutation: {
+		addFolder: (_, { input }, { user: { _id } }) =>
+			new Folder({ ...input, user: _id })
+				.save()
+				.then(folder => folder.populate('user team documents').execPopulate()),
+		updateFolder: (_, { input }) => {
+			const { id } = input;
+			return Folder.findById(id).then(folder => {
+				if (folder) {
+					return Folder.findByIdAndUpdate(
+						{ _id: id },
+						{ $set: input },
+						{ new: true }
+					).populate('user team documents');
+				} else {
+					throw new ValidationError("Folder doesn't exist");
+				}
+			});
+		},
+		deleteFolder: (_, { input: { id } }) =>
+			Folder.findById(id).then(async folder => {
+				if (folder) {
+					await Folder.findByIdAndDelete({ _id: id });
+					await Document.deleteMany({ folder: folder._id });
+				} else {
+					throw new ValidationError("Folder doesn't exist.");
+				}
+			})
 	}
 };
 
--- backend/models/Document.js
+++ backend/models/Document.js
@@ -1,16 +1,17 @@
 const { Schema, model } = require('mongoose');
 
-const Document = new Schema({
-	doc_url: {
-		type: String,
-		required: true
+const Document = new Schema(
+	{
+		doc_url: {
+			type: String,
+			required: true
+		},
+		user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+		folder: { type: Schema.Types.ObjectId, ref: 'Folder', required: true },
+		title: { type: String, trim: true, required: true },
+		content: { type: String }
 	},
-	user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-	folder: { type: Schema.Types.ObjectId, ref: 'Folder', required: true },
-	title: { type: String, trim: true, required: true },
-	content: { type: String, required: true },
-	createdAt: Schema.Types.Date,
-	updatedAt: Schema.Types.Date
-});
+	{ timestamps: true }
+);
 
 module.exports = model('Document', Document);
