039347cd97f4dcfb55cc9b4b1fb431df0f75d7a4
steelworx <eileencuevas62@gmail.com>
2019-02-20 14:09:37 -0500

    renamed folder that houses document files and refactored folders.js

--- frontend/src/Content/DocumentsTab/Folders.js
+++ frontend/src/Content/DocumentsTab/Folders.js
@@ -1,30 +1,51 @@
 import React, { Component } from 'react';
 import { Query } from 'react-apollo';
+import styled from 'styled-components';
 import * as query from '../../constants/queries';
 import { addFolder } from '../mutations/folders';
-import styled from 'styled-components';
 
-const Container = styled.div`
-	width: 80%;
-	border: 1px solid white;
-	margin: 0 auto;
+const FolderContainer = styled.div`
 	display: flex;
 	flex-wrap: wrap;
 	justify-content: center;
+`;
 
-	p {
-		color: white;
-		text-align: center;
-		padding: 10px;
-		border: 1px solid white;
-		margin: 10px;
-	}
+const IndividualFolder = styled.p`
+	min-height: 200px;
+	color: black;
+	background-color: white;
+	text-align: center;
+	padding: 40px 10px 10px 10px;
+	border: 1px solid white;
+	border-radius: 5px;
+	margin: 10px;
+	clip-path: polygon(
+		0% 0%,
+		45% 0%,
+		55% 10%,
+		100% 10%,
+		99% 10%,
+		100% 11%,
+		100% 100%,
+		0% 100%
+	);
 `;
 
 class Folders extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			displayButtons: false
+		};
+	}
+
+	toggleButtons = () => {
+		this.setState({ displayButtons: !this.state.displayButtons });
+	};
+
 	render() {
 		return (
-			<Container>
+			<FolderContainer>
 				<Query
 					query={query.FIND_FOLDERS_BY_TEAM}
 					variables={{ team: this.props.team._id }}
@@ -34,14 +55,16 @@ class Folders extends Component {
 						if (error) return console.error(error);
 						if (findFoldersByTeam && findFoldersByTeam.length > 0) {
 							return findFoldersByTeam.map(folder => (
-								<p key={folder._id}>{folder.title}</p>
+								<IndividualFolder key={folder._id}>
+									{folder.title}
+								</IndividualFolder>
 							));
 						} else {
 							return <p>No Documents Available For This Team</p>;
 						}
 					}}
 				</Query>
-			</Container>
+			</FolderContainer>
 		);
 	}
 }
