7cb7dd17a9b8dccac37d7d012d5b4c26869057bc
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2019-03-03 22:57:31 -0800

    adds folder VC swift files

---
+++ ios/TeamHome/TeamHome/View Controllers/FolderDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FolderDetailViewController.swift
+//  TeamHome
+//
+//  Created by Jonathan T. Miles on 3/3/19.
+//  Copyright © 2019 Lambda School under the MIT license. All rights reserved.
+//
+
+import UIKit
+
+class FolderDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
