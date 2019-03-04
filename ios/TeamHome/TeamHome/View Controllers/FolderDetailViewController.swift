f3cea905690c43435a95a82b48d796702ef65473
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2019-03-04 14:09:45 -0800

    adds logic for folders displayed in list, paralleling the situation for documents tableView

--- ios/TeamHome/TeamHome/View Controllers/FolderDetailViewController.swift
+++ ios/TeamHome/TeamHome/View Controllers/FolderDetailViewController.swift
@@ -7,24 +7,62 @@
 //
 
 import UIKit
+import Apollo
 
-class FolderDetailViewController: UIViewController {
+class FolderDetailViewController: UIViewController, TabBarChildrenProtocol {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setUpViewAppearance()
+        createGradientLayer()
+        teamNameLabel.textColor = .white
+        teamNameLabel.font = Appearance.setTitleFont(with: .title2, pointSize: 20)
+        
+        // Show team name on label
+        displayTeamInfo()
     }
     
+    // MARK: - Private Functions
+    // Create gradient layer for view background.
+    private func createGradientLayer() {
+        gradientLayer = CAGradientLayer()
+        
+        gradientLayer.frame = self.view.bounds
+        
+        gradientLayer.colors = [Appearance.grayColor.cgColor, Appearance.likeGrayColor.cgColor, Appearance.grayColor.cgColor]
+        
+        
+        gradientLayer.locations = [0.0, 0.5]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    private func displayTeamInfo() {
+        guard let team = team else { return }
+        
+        teamNameLabel.text = team.name
+    }
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "EmbeddedTable"{
+            let destinationVC = segue.destination as! DocumentsTableViewController
+            destinationVC.apollo = apollo
+            destinationVC.team = team
+        }
     }
-    */
+    
+    // MARK: - Properties
+    private var gradientLayer: CAGradientLayer!
+    @IBOutlet weak var teamNameLabel: UILabel!
+    
+    
+    var team: FindTeamsByUserQuery.Data.FindTeamsByUser?
+    var apollo: ApolloClient?
+    var currentUser: CurrentUserQuery.Data.CurrentUser?
 
 }
--- ios/TeamHome/TeamHome/View Controllers/FoldersTableViewController.swift
+++ ios/TeamHome/TeamHome/View Controllers/FoldersTableViewController.swift
@@ -7,52 +7,42 @@
 //
 
 import UIKit
+import Apollo
 
 class FoldersTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        tableView.backgroundColor = .clear
+        loadFolders(with: apollo!)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let watcherFolder = watcherFolder {
+            watcherFolder.refetch()
+        }
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return folders?.count ?? 0
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FolderCell") else {return UITableViewCell()}
+        cell.backgroundColor = .clear
+            guard let folder = folders?[indexPath.row] else { return cell }
+            cell.textLabel?.text = folder.title
+            // cell.detailTextLabel?.text = document.docUrl // --> this could list folder contents, e.g.
+            return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
     // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
@@ -60,22 +50,26 @@ class FoldersTableViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    // MARK: - Private Functions
+    
+    private func loadFolders(with apollo: ApolloClient) {
+        
+        guard let team = team,
+            let teamID = team.id else { return }
+        
+        // note: watcherFolder is declared in DocumentsTableViewController.swift
+        watcherFolder = apollo.watch(query: FindFoldersByTeamQuery(teamID: teamID)) { (result, error) in
+            if let error = error {
+                NSLog("Error loading Folders: \(error)")
+            }
+            
+            guard let result = result,
+                let folders = result.data?.findFoldersByTeam else { return }
+            
+            self.folders = folders
+        }
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
@@ -86,5 +80,22 @@ class FoldersTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    // MARK: - Properties
+    
+    var folders: [FindFoldersByTeamQuery.Data.FindFoldersByTeam?]? {
+        didSet {
+            if isViewLoaded {
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+    
+    var apollo: ApolloClient!
+    var team: FindTeamsByUserQuery.Data.FindTeamsByUser!
+    var currentUser: CurrentUserQuery.Data.CurrentUser?
+    var deleteIndexPath: IndexPath?
 
 }
