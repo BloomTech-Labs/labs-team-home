9ee12b0451b60d12d9551d7dc9afc9af58bc42f5
Nedim O <civoremon@gmail.com>
2019-03-07 12:24:41 -0500

    Update ActivityTimeline.js

--- frontend/src/Content/ActivityTimeline/ActivityTimeline.js
+++ frontend/src/Content/ActivityTimeline/ActivityTimeline.js
@@ -1,155 +1,102 @@
-/* activitySwitch changes the message depending on the type of the object */
-// const objectSwitch = type => {
-// 	switch (type) {
-// 		case 'Message':
-// 			return 'Message';
-// 		case 'Comment':
-// 			return 'Comment';
-// 		case 'Document':
-// 			return 'Document';
-// 		case 'Folder':
-// 			return 'Folder';
-// 		default:
-// 			return 'Comment';
-// 	}
-// };
-
 import React from 'react';
-
-// ------------- gql Imports ---------------------- //
 import { Query } from 'react-apollo';
-import * as query from '../../constants/queries';
+import { FIND_EVENTS_BY_TEAM } from '../../constants/queries';
+import Activity from './Activity';
+import ActivityModal from './ActivityModal';
 
-// // ------------- Component Imports ---------------------- //
-// import DocumentDetails from '../Documents/DocumentDetails';
-
-// ------------- Style Imports ---------------------- //
-import styled from 'styled-components';
-import CloseIcon from '@material-ui/icons/Close';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import Avatar from '@material-ui/core/Avatar';
-import { colors } from '../../colorVariables';
-
-// ------------- Modal styling imports ---------------------- //
-import {
-	StyledModal,
-	StyledModalOverlay,
-	StyledModalClose,
-	StyledModalPaper,
-	StyledModalTitle,
-	StyledModalButton,
-	StyledModalForm,
-	StyledModalInput,
-	StyledModalCardAction,
-	StyledModalBody,
-	StyledModalIconButton
-} from '../Modal.styles';
-
-// ---------------- Styled Components ---------------------- //
-
-const ModalTitle = styled(StyledModalTitle)`
-	h2 {
-		font-size: 30px;
-		text-align: center;
-		margin-left: 30px;
-	}
-`;
-
-const SmallerTitle = styled(ModalTitle)`
-	h2 {
-		font-size: 18px;
-	}
-`;
-
-const DocumentTitle = styled(StyledModalTitle)`
-	h2 {
-		font-size: 18px;
-		margin-left: 15px;
-	}
-`;
-
-class EventDetails extends React.Component {
+export default class ActivityTimeline extends React.Component {
 	constructor(props) {
 		super(props);
+
 		this.state = {
-			title: '',
-			editingFolder: false,
-			documentDetailOpen: false,
-			currentDocument: null,
-			refreshed: false
+			team: props.team,
+			eventDetailOpen: false,
+			currentEvent: null
 		};
 	}
 
-	render() {
-		const { hideModal, open, event } = this.props;
+	toggleEventDetail = event => {
+		this.setState(prevState => ({
+			eventDetailOpen: !prevState.eventDetailOpen,
+			currentEvent: event
+		}));
+	};
 
-		if (event === null) return <></>;
+	render() {
+		console.log('currenct props from activity timeline: ', this.props);
 		return (
-			<StyledModal
-				open={open}
-				onClose={() => {
-					hideModal();
-					// this.resetState();
-				}}
-			>
-				<StyledModalClose>
-					<StyledModalIconButton
-						onClick={() => {
-							hideModal();
-							// this.resetState();
-						}}
-					>
-						<CloseIcon />
-					</StyledModalIconButton>
-				</StyledModalClose>
-				<StyledModalOverlay>
-					<ModalTitle>Event</ModalTitle>
-					{/* The header information: name, avatar */}
-					<StyledModalPaper>
-						<CardHeader
-							avatar={
-								<Avatar
-									src={event.user.avatar}
-									alt="avatar"
-									style={{ height: '50px', width: '50px' }}
-								/>
-							}
-							title={`${event.user.firstName} ${event.user.lastName}`}
-							titleTypographyProps={{
-								style: { color: colors.text }
-							}}
-						/>
-						<CardContent>
-							<StyledModalBody paragraph component="p">
-								{event.user.firstName} {event.action_string} a{' '}
-								{event.object_string} on {event.createdAt.toDateString()}
-							</StyledModalBody>
-							{/* <StyledModalCardAction>
-								{event.action_string === 'deleted' ? (
-									<StyledModalTitle>
-										{`This ${event.object_string} no longer exists.`}
-									</StyledModalTitle>
-								) : (
-									<StyledModalButton>
-										{`View ${event.object_string}`}
-									</StyledModalButton>
-								)}
-							</StyledModalCardAction> */}
-						</CardContent>
-					</StyledModalPaper>
-				</StyledModalOverlay>
-				{/* // Modals */}
-				{/* <DocumentDetails
-					open={this.state.documentDetailOpen}
-					hideModal={() => this.toggleDocumentDetail(null)}
-					document={this.state.currentDocument}
-					currentUser={currentUser}
+			<div>
+				{/* Queries for all Events, refected every 5000ms (5 seconds)*/}
+				<Query
+					query={FIND_EVENTS_BY_TEAM}
+					variables={{ team: this.state.team._id }}
+					pollInterval={5000}
+				>
+					{({ loading, error, data: { findEventsByTeam } }) => {
+						if (loading) return <p>Loading...</p>;
+						if (error) return <p>Error!</p>;
+
+						if (findEventsByTeam && findEventsByTeam.length > 0) {
+							findEventsByTeam.map(event => {
+								if (typeof event.createdAt === 'string' && event.createdAt) {
+									event.createdAt = new Date(parseInt(event.createdAt, 10));
+								}
+								return event;
+							});
+
+							findEventsByTeam.sort((a, b) => {
+								if (a.createdAt < b.createdAt) return 1;
+								if (a.createdAt > b.createdAt) return -1;
+								return 0;
+							});
+
+							return findEventsByTeam.map((event, index) => {
+								if (event.user !== null) {
+									if (event.user._id === this.props.currentUser._id) {
+										return (
+											<Activity
+												event={event}
+												key={index}
+												own="true"
+												clickHandler={e => {
+													e.preventDefault();
+													this.toggleEventDetail(event);
+												}}
+											/>
+										);
+									}
+									return (
+										<Activity
+											event={event}
+											key={index}
+											own="false"
+											clickHandler={e => {
+												e.preventDefault();
+												this.toggleEventDetail(event);
+											}}
+										/>
+									);
+								}
+								return (
+									<div key={index}>
+										There is an event here but it was not recorded properly
+									</div>
+								);
+							});
+						} else {
+							return <div> No events </div>;
+						}
+					}}
+				</Query>
+				<ActivityModal
+					open={this.state.eventDetailOpen}
+					hideModal={() => this.toggleEventDetail(null)}
+					event={this.state.currentEvent}
+					currentUser={this.props.currentUser}
 					team={this.props.team}
-				/> */}
-			</StyledModal>
+					stopProp={e => e.stopPropagation()}
+				/>
+			</div>
 		);
 	}
 }
-
-export default EventDetails;
