d4683c991c49365685aa932eb7dd47e25528ad1d
tryingtokeepup <kl.hawaii@gmail.com>
2019-03-11 13:21:39 -0700

    today's not the day i get tests working. oh well.

--- backend/__tests__/mock.js
+++ backend/__tests__/mock.js
@@ -1,19 +1,56 @@
-import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
-import { graphql } from 'graphql';
+import {
+	makeExecutableSchema,
+	addMockFunctionsToSchema,
+	mockServer
+} from 'graphql-tools';
 
-// Fill this in with the schema string
-const schemaString = `...`;
+const testCaseA = {
+	id: 'Test case A',
+	query: `
+    query {
+      animals {
+         origin
+      }
+    }
+  `,
+	variables: {},
+	context: {},
+	expected: { data: { animals: [{ kind: 'Dog' }] } }
+};
 
-// Make a GraphQL schema with no resolvers
-const schema = makeExecutableSchema({ typeDefs: schemaString });
+describe('Schema', () => {
+	// Array of case types
+	const cases = [testCaseA];
 
-// Add mocks, modifies schema in place
-addMockFunctionsToSchema({ schema });
+	const mockSchema = makeExecutableSchema({ typeDefs });
 
-const query = `
-query tasksForUser {
-  user(id: 6) { id, name }
-}
-`;
+	// Here we specify the return payloads of mocked types
+	addMockFunctionsToSchema({
+		schema: mockSchema,
+		mocks: {
+			Boolean: () => false,
+			ID: () => '1',
+			Int: () => 1,
+			Float: () => 12.34,
+			String: () => 'Dog'
+		}
+	});
 
-graphql(schema, query).then(result => console.log('Got result', result));
+	test('has valid type definitions', async () => {
+		expect(async () => {
+			const MockServer = mockServer(typeDefs);
+
+			await MockServer.query(`{ __schema { types { name } } }`);
+		}).not.toThrow();
+	});
+
+	cases.forEach(obj => {
+		const { id, query, variables, context: ctx, expected } = obj;
+
+		test(`query: ${id}`, async () => {
+			return await expect(
+				graphql(mockSchema, query, null, { ctx }, variables)
+			).resolves.toEqual(expected);
+		});
+	});
+});
