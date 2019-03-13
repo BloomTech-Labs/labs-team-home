59f1219ebd897b2e7c5216d287f33372857192c8
steelworx <eileencuevas62@gmail.com>
2019-03-13 16:55:46 -0400

    added more items to landing page

--- frontend/src/LandingView/components/BottomContent.js
+++ frontend/src/LandingView/components/BottomContent.js
@@ -1,11 +1,57 @@
 import React from 'react';
+import styled from 'styled-components';
 import {
 	LandingContent,
 	LandingContentContainer,
-	VideoContainer
+	VideoContainer,
+	Circle,
+	CircleDiv,
+	LineDiv,
+	BenefitsDiv,
+	IntegrationDiv,
+	BenefitsContainer,
+	Placeholder,
+	FirstPane,
+	TextDiv,
+	BenefitsCard
 } from '../styles/BottomStyled';
 import frontpage from '../../assets/demo.mp4';
 import Pricing from '../components/Pricing';
+import Button from '@material-ui/core/Button';
+import { Dropbox } from 'styled-icons/boxicons-logos/Dropbox';
+import { GoogleDrive } from 'styled-icons/fa-brands/GoogleDrive';
+import { Apple } from 'styled-icons/boxicons-logos/Apple';
+import { Onedrive } from 'styled-icons/icomoon/Onedrive';
+import Footer from './footer';
+import { colors } from '../../colorVariables';
+
+const StyledDropbox = styled(Dropbox)`
+	height: 75px;
+`;
+
+const StyledGDrive = styled(GoogleDrive)`
+	height: 75px;
+`;
+
+const StyledApple = styled(Apple)`
+	height: 75px;
+`;
+
+const StyledOnedrive = styled(Onedrive)`
+	height: 75px;
+`;
+const ModifiedButton = styled(Button)`
+	background-color: ${colors.button};
+	width: 200px;
+	margin: 10px 11%;
+	color: ${colors.text};
+	transition: background-color 250ms ease-in-out, transform 150ms ease;
+	&:hover {
+		background-color: rgba(255, 255, 255, 0.7);
+		color: black;
+	}
+`;
+
 const BottomContent = props => {
 	return (
 		<>
@@ -13,14 +59,15 @@ const BottomContent = props => {
 				<LandingContent>
 					{/* This is where the homepage basically lies. Hopefully, we can style this to look slightly better.  */}
 					<h1>
-						Share. <span>Work.</span> Congregate.
+						Connect. <span>Organize. </span> Collaborate.
 					</h1>
 					<br />
 					<p>
-						Communicating within a team can be a pain. Let's fix that for you.
-						Introducing Sveza, a better way to share thoughts in a group
-						setting.
+						{' '}
+						No more missing a conversation. No more missed opportunities. Sveza
+						is here.{' '}
 					</p>
+					<ModifiedButton>Sign Up Now</ModifiedButton>
 				</LandingContent>
 				<VideoContainer>
 					<video id="video" autoPlay loop muted>
@@ -28,7 +75,78 @@ const BottomContent = props => {
 					</video>
 				</VideoContainer>
 			</LandingContentContainer>
+			<FirstPane>
+				<TextDiv>
+					<p>
+						Organizing a team is hard. Every person has their preferred work
+						environment and preferred apps. They operate on their own timelines,
+						on their own schedules. And they all think about work differently.
+					</p>
+					<p>
+						{' '}
+						At Sveza, our aim is to get your team operating from one place, so
+						that no member of the team is left behind, while each member gets to
+						operate out of the environment they are most comfortable working in.{' '}
+					</p>
+				</TextDiv>
+				<Placeholder />
+			</FirstPane>
+			<BenefitsDiv>
+				<h2>Benefits</h2>
+				<BenefitsContainer>
+					<BenefitsCard>
+						<div>
+							<h3>Ease of use</h3>
+						</div>
+						<div>
+							<p>Sveza is out of the box and ready to use. Right now. </p>
+						</div>
+					</BenefitsCard>
+					<BenefitsCard>
+						<div>
+							<h3>Simplicity</h3>
+						</div>
+						<div>
+							<p>
+								Sveza distills out the noise and keeps the information load
+								light.{' '}
+							</p>
+						</div>
+					</BenefitsCard>
+					<BenefitsCard>
+						<div>
+							<h3>Adaptability</h3>
+						</div>
+						<div>
+							<p>Sveza can incorporate a wide range workplace environments. </p>
+						</div>
+					</BenefitsCard>
+				</BenefitsContainer>
+			</BenefitsDiv>
+			<IntegrationDiv>
+				<p>
+					In addition to its own built in tracking, commenting, and messaging
+					system, Sveza integrates with everything from the Creative Cloud, to
+					Drop Box, to Google Docs and more.{' '}
+				</p>
+				<CircleDiv>
+					<LineDiv />
+					<Circle>
+						<StyledDropbox />
+					</Circle>
+					<Circle>
+						<StyledOnedrive />
+					</Circle>
+					<Circle>
+						<StyledGDrive />
+					</Circle>
+					<Circle>
+						<StyledApple />
+					</Circle>
+				</CircleDiv>
+			</IntegrationDiv>
 			<Pricing />
+			<Footer />
 		</>
 	);
 };
